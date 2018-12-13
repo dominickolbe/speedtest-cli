8712a6afd99e0337918fa88032eb2d2ae3eb25d4
Dominic Kolbe <kolbedominic@gmail.com>
2018-12-13 15:01:12 +0100

    update script, add IP and test server

--- src/index.js
+++ src/index.js
@@ -16,27 +16,25 @@ const stats = {
 };
 
 const render = finished => {
-
   const placeholder = '...';
   const ping = stats.ping ? `${stats.ping}ms` : placeholder;
   const download = stats.download ? `${stats.download.toFixed()} Mbit/s (${(stats.download * multiplier).toFixed()} MB/s)` : placeholder;
   const upload = stats.upload ? `${Math.round(stats.upload)} Mbit/s (${Math.round(stats.upload * multiplier)} MB/s)` : placeholder;
+  const info = finished ? `Your IP: ${stats.client.ip} | Testserver: ${stats.server.host} in ${stats.server.location} ${stats.server.country}` : '';
 
-  spinner.text = `${stats.finished ? 'Test done' : 'In progress ...'}
+  spinner.text = `${finished ? 'Test done': 'In progress ...'}
 
     Ping: ${ping}
 Download: ${download}
   Upload: ${upload}
+
+${info}
 `;
 };
 
 render();
 
 const test = speedTest({ maxTime: 20000 });
-
-// test.on('data', speed => {
-//   render();
-// });
 
 // test.on('downloadspeedprogress', speed => {
 //   render();
@@ -61,14 +59,16 @@ test.on('uploadspeed', speed => {
   render();
 });
 
-test.on('data', data => {
-  render(false);
+test.once('data', data => {
+  stats.client.ip = data.client.ip;
+  stats.server = data.server;
+  render(true);
   spinner.succeed();
   process.exit();
 });
 
 test.on('error', err => {
-  render(false);
+  render(true);
   spinner.fail();
   console.error(err);
   process.exit(1);

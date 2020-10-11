89f8b6708095e7c4474a55b365e61ee4d15784b0
Dominic Kolbe <kolbedominic@gmail.com>
2020-10-11 02:17:16 +0100

    CS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-import ora from 'ora';
-import chalk from 'chalk';
-import speedTest from 'speedtest-net';
+import ora from "ora";
+import speedTest from "speedtest-net";
 
 const spinner = new ora();
 const multiplier = 1 / 8;
@@ -15,14 +14,24 @@ const stats = {
   ping: null,
 };
 
-const render = finished => {
-  const placeholder = '...';
+const render = (finished) => {
+  const placeholder = "...";
   const ping = stats.ping ? `${stats.ping}ms` : placeholder;
-  const download = stats.download ? `${(stats.download).toFixed(2)} Mbit/s (${(stats.download * multiplier).toFixed(2)} MB/s)` : placeholder;
-  const upload = stats.upload ? `${(stats.upload).toFixed(2)} Mbit/s (${(stats.upload * multiplier).toFixed(2)} MB/s)` : placeholder;
-  const info = finished ? `Your IP: ${stats.client.ip} | Testserver: ${stats.server.host} in ${stats.server.location} ${stats.server.country}` : '';
+  const download = stats.download
+    ? `${stats.download.toFixed(2)} Mbit/s (${(
+        stats.download * multiplier
+      ).toFixed(2)} MB/s)`
+    : placeholder;
+  const upload = stats.upload
+    ? `${stats.upload.toFixed(2)} Mbit/s (${(stats.upload * multiplier).toFixed(
+        2
+      )} MB/s)`
+    : placeholder;
+  const info = finished
+    ? `Your IP: ${stats.client.ip} | Testserver: ${stats.server.host} in ${stats.server.location} ${stats.server.country}`
+    : "";
 
-  spinner.text = `${finished ? 'Test done': 'In progress ...'}
+  spinner.text = `${finished ? "Test done" : "In progress ..."}
 
     Ping: ${ping}
 Download: ${download}
@@ -36,32 +45,32 @@ render();
 
 const test = speedTest({ maxTime: 20000 });
 
-test.on('downloadspeedprogress', speed => {
+test.on("downloadspeedprogress", (speed) => {
   stats.download = speed;
   render();
 });
 
-test.on('uploadspeedprogress', speed => {
+test.on("uploadspeedprogress", (speed) => {
   stats.upload = speed;
   render();
 });
 
-test.once('testserver', server => {
+test.once("testserver", (server) => {
   stats.ping = Math.round(server.bestPing);
   render();
 });
 
-test.on('downloadspeed', speed => {
+test.on("downloadspeed", (speed) => {
   stats.download = speed;
   render();
 });
 
-test.on('uploadspeed', speed => {
+test.on("uploadspeed", (speed) => {
   stats.upload = speed;
   render();
 });
 
-test.once('data', data => {
+test.once("data", (data) => {
   stats.client = data.client;
   stats.server = data.server;
   render(true);
@@ -69,7 +78,7 @@ test.once('data', data => {
   process.exit();
 });
 
-test.on('error', err => {
+test.on("error", (err) => {
   spinner.fail();
   console.error(err);
   process.exit(1);
